40a9141d5fc047f7ced199619fab9b6f2dedac5b
Vikas Yadav <vikashyadav62502@gmail.com>
2024-12-26 19:44:56 +0530

    admin side wallet update funtionality done

--- src/components/UserDetailModal.tsx
+++ src/components/UserDetailModal.tsx
@@ -1,7 +1,28 @@
 // UserDetailsModal.js
+import { useState } from "react";
 import close from "../assets/close.png";
+import client from "../utils/axiosClient";
+import { toast } from "sonner";
 
-const UserDetailsModal = ({ selectedUser, closeModal }:any) => {
+const UserDetailsModal = ({ selectedUser, closeModal }: any) => {
+  const [amount, setAmount] = useState<number>();
+  const handleBalanceUpdate = async (type: string, userId: string) => {
+    const toastLoader = toast.loading("Updating balance...");
+    try {
+      const res = await client.post(`/api/admin/update-user-balance${userId}`, {
+        amount: amount,
+        type,
+      });
+      toast.success(res.data.message);
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to update balance. Please try again.");
+    } finally {
+      toast.dismiss(toastLoader);
+      closeModal();
+    }
+  }
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white gap-4 rounded-lg shadow-lg p-6 pt-0 lg:w-[40%] w-[90%]">
@@ -24,7 +45,7 @@ const UserDetailsModal = ({ selectedUser, closeModal }:any) => {
                 <strong>Mob No.</strong>: +91-{selectedUser.mobile}
               </p>
               <p>
-                <strong>Wallet Balance</strong>:Rs {selectedUser.walletBalance} 
+                <strong>Wallet Balance</strong>:Rs {selectedUser.walletBalance}
               </p>
             </div>
           ) : (
@@ -35,12 +56,14 @@ const UserDetailsModal = ({ selectedUser, closeModal }:any) => {
               type="number"
               placeholder="Enter amount"
               className="w-full p-2 border rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              value={amount}
+              onChange={(e) => setAmount(+e.target.value)}
             />
             <div className="flex gap-4 mt-4">
-              <button className="flex-1 py-2 font-semibold text-lg bg-green-500 text-white rounded hover:bg-green-600">
+              <button className="flex-1 py-2 font-semibold text-lg bg-green-500 text-white rounded hover:bg-green-600" onClick={() => handleBalanceUpdate("credit", selectedUser?._id)}>
                 Credit
               </button>
-              <button className="flex-1 py-2 font-semibold text-lg bg-red-500 text-white rounded hover:bg-red-600">
+              <button className="flex-1 py-2 font-semibold text-lg bg-red-500 text-white rounded hover:bg-red-600" onClick={() => handleBalanceUpdate("debit", selectedUser?._id)} >
                 Debit
               </button>
             </div>
